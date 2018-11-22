9871226c87c602d7e6cd657acb34241606df042b
Mael MAINCHAIN <mael@mainchain.fr>
2018-11-22 15:17:23 +0100

    Feat: initialisation of project

---
+++ app/src/main/java/com/mainchain/mael/android_native_quick_start/LibraryActivity.java
@@ -0,0 +1,13 @@
+package com.mainchain.mael.android_native_quick_start;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LibraryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library);
+    }
+}
