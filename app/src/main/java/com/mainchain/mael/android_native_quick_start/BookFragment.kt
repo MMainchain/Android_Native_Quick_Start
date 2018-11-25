d644a1af905812b8fe112d394d3f76b29f6f2c85
Mael MAINCHAIN <mael@mainchain.fr>
2018-11-25 18:09:47 +0100

    Feat: design book view

--- app/src/main/java/com/mainchain/mael/android_native_quick_start/BookFragment.kt
+++ app/src/main/java/com/mainchain/mael/android_native_quick_start/BookFragment.kt
@@ -5,8 +5,10 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import com.mainchain.mael.android_native_quick_start.entities.Book
+import com.squareup.picasso.Picasso
 
 class BookFragment : Fragment() {
 
@@ -23,12 +25,19 @@ class BookFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val textView = view.findViewById<TextView>(R.id.book_number)
-        textView.text = "Hello world"
+        val bookCover = view.findViewById<ImageView>(R.id.book_cover)
+        val bookTitle = view.findViewById<TextView>(R.id.book_title)
+        val bookPrice = view.findViewById<TextView>(R.id.book_price)
+        val bookSynopsis = view.findViewById<TextView>(R.id.book_synopsis)
+        bookTitle.text = "Sélectionnez un livre dans l'écran de gauche"
 
         if (book !== null) {
-            textView.text = this.book?.title
-            // view.findViewById<TextView>(R.id.book_content).text = "Je suis là"
+            bookTitle.text = this.book?.title
+            Picasso.get().load(this.book?.cover).into(bookCover)
+            bookPrice.text = "Prix : " + this.book?.price + "€"
+            var synopsis = ""
+            this.book?.synopsis?.forEach { part -> synopsis += part }
+            bookSynopsis.text = synopsis
         }
     }
 
